c5057b7f2da77f0bb571824575625537cbe8c74a
Abdullah-Younus <abdullah1001k@gmail.com>
2022-11-21 17:21:12 +0500

    commit

---
+++ functions/app.ts
@@ -0,0 +1,36 @@
+// any
+
+let hero: string;
+
+function getHero() {
+    return "thore";
+}
+
+hero = getHero();
+
+
+
+function addTwo(num: number) {
+    return num + 2;
+}
+
+function getUpperCase(val: string) {
+    return val.toUpperCase();
+}
+
+
+addTwo(4);
+getUpperCase("Sameer");
+
+
+
+
+
+
+
+
+
+
+
+
+export { }

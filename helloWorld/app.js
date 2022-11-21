ee6d5c018523187bb974be53a3f70df04d322e1a
Abdullah-Younus <abdullah1001k@gmail.com>
2022-11-21 17:16:04 +0500

    edit

---
+++ helloWorld/app.js
@@ -0,0 +1,12 @@
+var message = 'Hello World';
+var userName = "Abdullah";
+var heading = document.createElement('h1');
+heading.textContent = message;
+document.body.appendChild(heading);
+console.log('Message :', message);
+console.log('UserName :', userName);
+var user = {
+    name: 'Abdullah',
+    age: 18
+};
+console.log('User :', user);
---
+++ helloWorld/app.ts
@@ -0,0 +1,18 @@
+let message: string = 'Hello World';
+let userName: string = "Abdullah";
+
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+document.body.appendChild(heading);
+
+console.log('Message :', message);
+console.log('UserName :', userName);
+
+
+let user = {
+    name: 'Abdullah',
+    age: 18
+}
+
+console.log('User :', user);

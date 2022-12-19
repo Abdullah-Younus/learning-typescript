c4f80188996adb699087a4e0c6b010626a247f28
Abdullah Younus <abdullah1001k@gmail.com>
2022-12-19 14:48:47 +0500

    eum

---
+++ enum/index.js
@@ -0,0 +1,27 @@
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+; /// start with 0 like array index
+var c = Color.Green; /// print index number 1
+console.log('C ======>', c);
+var Color1;
+(function (Color1) {
+    Color1[Color1["Red"] = 1] = "Red";
+    Color1[Color1["Green"] = 2] = "Green";
+    Color1[Color1["Blue"] = 3] = "Blue";
+})(Color1 || (Color1 = {}));
+;
+var colorName = Color1[2]; /// print the value of Color
+console.log('color Name ====> ', colorName);
+var Color2;
+(function (Color2) {
+    Color2[Color2["Red"] = 1] = "Red";
+    Color2[Color2["Green"] = 2] = "Green";
+    Color2[Color2["Blue"] = 4] = "Blue";
+})(Color2 || (Color2 = {})); // can assign value to all
+var colorIndex = Color2["Blue"];
+console.log("Color Index :=======>", colorIndex);
+export {};
---
+++ enum/index.ts
@@ -0,0 +1,20 @@
+enum Color { Red, Green, Blue }; /// start with 0 like array index
+
+
+var c: Color = Color.Green /// print index number 1
+
+console.log('C ======>', c);
+
+
+enum Color1 { Red = 1, Green, Blue };
+
+var colorName: string = Color1[2]; /// print the value of Color
+
+console.log('color Name ====> ', colorName);
+
+
+enum Color2 { Red = 1, Green = 2, Blue = 4 } // can assign value to all
+
+var colorIndex = Color2["Blue"];
+
+console.log("Color Index :=======>", colorIndex);

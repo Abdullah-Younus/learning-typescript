88ba8c1ae8b1dd5b6650352d808b8e381e07ef1d
Abdullah Younus <abdullah1001k@gmail.com>
2022-12-19 15:05:24 +0500

    array

--- array/app.ts
+++ array/app.ts
@@ -1,3 +1,20 @@
+let array1: number[] = [5, 6, 7, 8, 9, 10]; // correct syntax
+console.log(array1[3]); // correct syntax
+
+// alternative corrext syntax
+
+let arry2: Array<number> = [1, 2, 5];
+
+let arry3: number[] = [];
+
+
+let array5: number[] = [];
+array5.push(45, 25); // dynamically array
+
+
+
+
+
 const superHeros: string[] = [];
 // const heroPower: number[] = [];  another way
 const heroPower: number[] = []

31c3f5fd48e0be140a783e303b7324ffd6b28cf9
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-23 02:37:54 +0500

    callbacks

--- functions/callbacks.ts
+++ functions/callbacks.ts
@@ -7,3 +7,15 @@ function callingFunction(initialText: string, callback: (text: string) => void) 
 }
 
 callingFunction("myText", myCallBack);
+
+
+function myback(text: string) {
+    console.log('Inside My Call Back :', text);
+}
+
+
+function call(name: string, myback: (text: string) => void) {
+    myback(name)
+}
+
+call('Abdullah', myback)

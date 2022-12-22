c8d3ee1be82c2280f6c1591e70c4eacc9af0bebc
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-23 02:50:21 +0500

    function overloads

---
+++ function_overloads/index.ts
@@ -0,0 +1,19 @@
+// overloads give us type-checked calls
+
+
+// its customary to order overloads from most specific to least specific
+
+function add(arg: string, arg2: string): string; // option 1
+function add(arg: string, arg2: number): number; // option 2
+function add(arg: string, arg2: string): boolean; // option 3
+
+
+function add(arg1: any, arg2: any): any {
+    return arg1 + arg2
+}
+
+
+console.log(add(1, 2));
+console.log(add("Hello", "World"));
+console.log(add(true, false));
+

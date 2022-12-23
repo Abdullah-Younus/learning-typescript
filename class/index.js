70987adb55a252ebb6f6b06ffb65aff16556c4f4
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-24 00:58:55 +0500

    class done

---
+++ class/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Point {
+    x;
+    y;
+}
+const pt = new Point();
+pt.x = "45";
+pt.y = "Abdullah";
+class BasGreeter {
+    name = "Sameer";
+    constructor() {
+    }
+}
+const obj1 = new BasGreeter();
+console.log('Obj ====>', obj1);
--- class/index.ts
+++ class/index.ts
@@ -9,3 +9,21 @@ pt.x = "45";
 pt.y = "Abdullah";
 
 
+class BasGreeter {
+    name: string = "Sameer";
+
+    constructor() {
+        this.name = "hello";
+    }
+}
+
+
+const obj1 = new BasGreeter();
+console.log('Obj ====>', obj1); // print constructor value
+
+
+
+
+
+
+

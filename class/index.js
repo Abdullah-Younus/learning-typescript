0c0588aac60ab2752cce091e07faafc7658401fa
Abdullah-Younus <abdullah1001k@gmail.com>
2022-12-24 01:53:42 +0500

    class and constructor

--- class/index.js
+++ class/index.js
@@ -10,7 +10,35 @@ pt.y = "Abdullah";
 class BasGreeter {
     name = "Sameer";
     constructor() {
+        this.name = "hello";
     }
 }
 const obj1 = new BasGreeter();
-console.log('Obj ====>', obj1);
+console.log('Obj ====>', obj1); // print constructor value
+// definite assignment assertion operator !
+class OkGreeter {
+    name;
+}
+const obj2 = new OkGreeter();
+obj2.name = "Khan";
+console.log('Obj2 =======>', obj2.name);
+class Student {
+    firstName;
+    lastName;
+    age;
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    getFirstName() {
+        console.log("My First Name Is :" + this.firstName);
+        console.log("My Age :" + this.age);
+    }
+    getSecondName(arg) {
+        console.log('My parameter is :', arg);
+    }
+}
+const obj6 = new Student("sameer", "khan", 45);
+obj6.getFirstName();
+obj6.getSecondName("45");
--- class/index.ts
+++ class/index.ts
@@ -24,6 +24,37 @@ console.log('Obj ====>', obj1); // print constructor value
 
 
 
+// definite assignment assertion operator !
 
+class OkGreeter {
+    name!: string;
+}
 
+const obj2 = new OkGreeter();
+obj2.name = "Khan"
+console.log('Obj2 =======>', obj2.name);
 
+class Student {
+    firstName: string;
+    lastName: string;
+    age: number
+
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    getFirstName() {
+        console.log("My First Name Is :" + this.firstName);
+        console.log("My Age :" + this.age);
+    }
+
+    getSecondName(arg: string) {
+        console.log('My parameter is :', arg);
+    }
+}
+
+const obj6 = new Student("sameer", "khan", 45);
+obj6.getFirstName();
+obj6.getSecondName("45");
